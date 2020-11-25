6f247743f97a7be957010cfcb815424246b3cd74
rymarinelli <ryanmarinelli@gmail.com>
2020-11-25 08:55:26 -0600

    Adding Leetcode problem

--- LeetCode/Reversed String.py
+++ LeetCode/Reversed String.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 25 08:46:33 2020
-
-@author: ryanm
-"""
-
-
 class Solution:
     def reverse(self, x: int) -> int:
         x = str(x)
@@ -22,18 +14,22 @@ class Solution:
         for i in reversed_val:
             result += i
         
+        # Look if negative
         if(result.endswith("-") == True):
             result = "-" + result[0:-1]
-            if(result[1] == "0"):
-                result = result[0] + result[2:]
             
-        
-        if(result.startswith("0") == True and len(result) > 1):
-            result = result[(result.rfind("0") + 1): len(result)]
-            print(result)
+            # Removes last zero and slices position after
+            if(result.startswith("0") == True and len(result) > 1):
+                result = result[(result.rfind("0") + 1): len(result)]
+                result = "-" + result[0:-1]
+                print(result)
             return(result)
         
-        
+        #If not negative
+        else:
+            if(result.startswith("0") == True and len(result) > 1):
+                result = result[(result.rfind("0") + 1): len(result)]
+                return(result)
             
         return(result)
         
