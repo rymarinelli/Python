18f28a0d0a1e9c0197cfd0aad04065c54f8be8f2
rymarinelli <ryanmarinelli@gmail.com>
2020-12-03 05:50:53 -0600

    Adding Converter

--- Tkinter/Final_Project-GUI.py
+++ Tkinter/Final_Project-GUI.py
@@ -8,6 +8,7 @@ Created on Mon Nov 30 20:32:06 2020
 from tkinter import * 
 from tkinter.ttk import *
 from tkinter.filedialog import askopenfile 
+from tkinter.filedialog import asksaveasfile 
 
 import pandas as pd
 import json
@@ -22,28 +23,36 @@ root.geometry('200x200')
 # will be opened 
 
 
-def selectFile(): 
-    file = askopenfile(mode ='r', filetypes =[('Comma-Delimited', '*.csv')]) 
-    if file is not None: 
-        content = file.read() 
-        csvFile = pd.read_csv(content)
-        df = pd.DataFrame(content)
-        jsonFile = df.to_json(orient="split")
-        json.dumps(jsonFile)
-        return(content) 
+class File():
     
-    def convertFile():
-        csvFile = pd.read_csv(content)
-        df = pd.DataFrame(content)
-        jsonFile = df.to_json(orient="split")
-        print(content)
-        return(jsonFile)
     
-       
-       
+    def __init__(self):
+        self.content = content
+    
+    def selectFile():
+        file = askopenfile(mode ='r', filetypes =[('Comma-Delimited', '*.csv')]) 
+        if file is not None: 
+           content = file.read() 
+           return(content)
+          
+     
+        def toJSON():    
+            content = File.selectFile()
+            csvFile = pd.read_csv(content)
+            df = pd.DataFrame(csvFile)
+            jsonFile = df.to_json(orient="split")
+            asksaveasfile(jsonFile, defaultextension = ".JSON") 
         
-convertButton = Button(root, text = "Convert", command = lambda:convertFile())      
-btn = Button(root, text ='Open', command = lambda:selectFile()) 
+        toJSON()
+       
+
+        
+convertButton = Button(root, text = "Convert", command = lambda: File.toJSON())      
+btn = Button(root, text ='Open', command = lambda: File.selectFile()) 
+btnTwo = ttk.Button(root, text = 'Save', command = lambda : save()) 
+btnTwo.pack(side = TOP, pady = 20) 
+  
+
 
 btn.pack(side = TOP, pady = 10) 
 convertButton.pack(side = TOP, pady = 10)
